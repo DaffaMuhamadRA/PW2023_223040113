a64fd370d7d5a0ef15785aad03444db1caba2bfd
Daffa Muhamad <Daffamuhamadra@gmail.com>
2023-05-08 22:37:51 +0700

    footer.php

---
+++ tubes/src/partials/footer/footer.php
@@ -0,0 +1,59 @@
+<footer class=" bg-dark text-white pt-5 pb-4">
+    <div class="container text-center text-md-left">
+        <div class="row text-center text-md-left">
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">FashiOn</h5>
+                <p>a company that stands in the fashion and clothing business that aims to
+                    provide comfort to the
+                    wearer in
+                    the beauty of clothing.</p>
+
+            </div>
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Product</h5>
+                <p><a href="#" class="text-white" style="text-decoration: none;">Men</a></p>
+                <p><a href="#" class="text-white" style="text-decoration: none;">Woman</a></p>
+                <p><a href="#" class="text-white" style="text-decoration: none;">Kid</a></p>
+            </div>
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Get In Touch</h5>
+                <p><a href="#" class="text-white" style="text-decoration: none;">Your
+                        Account</a></p>
+                <p><a href="#" class="text-white" style="text-decoration: none;">About</a></p>
+                <p><a href="#" class="text-white" style="text-decoration: none;">Contact</a></p>
+                <p><a href="#" class="text-white" style="text-decoration: none;">Help</a></p>
+            </div>
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Company Name</h5>
+                <p><i class="bi bi-house-fill me-3"></i>Indonesia</p>
+                <p><i class="bi bi-envelope-at-fill me-3"></i>Fashion@mail.com</p>
+                <p><i class="bi bi-telephone-fill me-3"></i>+82 34567890</p>
+                <p><i class="bi bi-telephone-fill me-3"></i>+11 22333444</p>
+            </div>
+        </div>
+        <hr class="mb-4">
+        <div class="row align-items-center">
+            <div class="col-md-7 col-lg-8">
+                <p>Copyright ©2022 All Right Reserver By :</p>
+                <!-- <a href="#" style="text-decoration: none"><strong class="text-danger bg-light"> Fashi<span
+                class="text-black">On </span></strong></a> -->
+                <a class="navbar-brand" href="#">
+                    <h2>HealthCare</h2>
+                </a>
+            </div>
+            <div class="col-md-5 col-lg-4">
+                <div class="text-center text-md right">
+                    <ul class="list-unstyled list-inline">
+                        <li class="list-inline-item">
+                            <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="bi bi-facebook"></i></a>
+                        </li>
+                        <li class="list-inline-item"><a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="bi bi-twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="bi bi-instagram"></i></a>
+                        </li>
+                        <li class="list-inline-item"><a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="bi bi-youtube"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
