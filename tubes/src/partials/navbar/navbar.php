7f9c49348a4200cb4699c848affb563b013fe22e
Daffa Muhamad <Daffamuhamadra@gmail.com>
2023-05-18 20:03:14 +0700

    UI kategori (on going)

--- tubes/src/partials/navbar/navbar.php
+++ tubes/src/partials/navbar/navbar.php
@@ -75,7 +75,6 @@
           <li><a class="dropdown-item" href="#"><i class="bi bi-person-bounding-box"></i> Your Account</a>
           </li>
           <li><a class="dropdown-item" href="#"><i class="bi bi-gear"></i> Setting</a></li>
-          <li><a class="dropdown-item" href="#"><i class="bi bi-people-fill"></i> Switch account</a></li>
           <li><a class="dropdown-item" href="#"><i class="bi bi-box-arrow-right"></i> Sign out</a></li>
         </ul>
       </div>
